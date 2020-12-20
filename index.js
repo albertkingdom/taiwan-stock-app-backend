861b99ef33f7a2b7b0053bda5af49812e3b40c58
albert <albertkingdom@gmail.com>
2020-12-20 10:15:58 +0800

    fix  cors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use("/stock", require(__dirname + "/routes/stock"));
 
