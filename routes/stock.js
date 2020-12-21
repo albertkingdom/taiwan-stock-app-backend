1147b15a2a2fc8866f96a717ffe11e2d63f08a49
Albert Lin <albertkingdom@gmail.com>
2020-12-21 15:59:20 +0800

    add router for kplot

--- routes/stock.js
+++ routes/stock.js
@@ -59,4 +59,32 @@ router.get("/hotstock", async (req, res) => {
   res.json({ hitoStocklist: response.data.data });
 });
 
+//kplot
+router.post("/kplot", async (req, res) => {
+  const { stockNo, date1, date2 } = req.body;
+  const url1 = `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${date1}&stockNo=${stockNo}`;
+  const url2 = `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${date2}&stockNo=${stockNo}`;
+  try {
+    async function getStockInfo(target) {
+      const response = await axios.get(target);
+
+      return response.data;
+    }
+    const response = Promise.all([getStockInfo(url1), getStockInfo(url2)]);
+    const [{ data: data1 }, { data: data2 }] = await response;
+
+    const modifydata = data1.concat(data2).slice();
+
+    modifydata.forEach((day) => {
+      day[0] = day[0].replace("109", "2020").replace(/\//g, "-");
+      day[1] = day[1].replace(/,/g, "");
+      day[2] = day[2].replace(/,/g, "");
+    });
+    // console.log("modifydata", modifydata);
+    res.json(modifydata);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
